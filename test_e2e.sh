690efa797f8b0598f79ac08fca80a1b46e175b39
manucarbonell <manucarbonell@github.com>
2020-02-18 15:39:40 +0100

    correct gt path

--- test_e2e.sh
+++ test_e2e.sh
@@ -1,2 +1,2 @@
-python test.py --model trained_models/iam_join_det_htr_csv_retinanet.pt --csv_val ../DATASETS/IAM/test_words.csv 
+python test.py --model trained_models/iam_join_det_htr_csv_retinanet.pt --csv_val datasets/iam/test_words.csv 
 
